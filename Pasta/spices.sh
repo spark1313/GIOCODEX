916230bd139388cf3b840de09a8a26d87e511881
spark1313 <57932291+spark1313@users.noreply.github.com>
2023-05-24 17:21:17 -0400

    Update spices.sh

--- Pasta/spices.sh
+++ Pasta/spices.sh
@@ -3,3 +3,6 @@ nohup <command>
 
 #Follow a log file line-by-line as it is generated
 tail -f file.log
+
+#Download something from the web (with the optional -O argument to specify a different filename for the downloaded file)
+wget -O newname.zip https://URL.domain/file.zip --progress=bar:force:noscroll
