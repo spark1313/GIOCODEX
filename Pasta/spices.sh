9dd61189fcc9cd78c83c71394320687eedf139d7
spark1313 <57932291+spark1313@users.noreply.github.com>
2023-05-24 17:24:28 -0400

    Update spices.sh

--- Pasta/spices.sh
+++ Pasta/spices.sh
@@ -6,3 +6,17 @@ tail -f file.log
 
 #Download something from the web (with the optional -O argument to specify a different filename for the downloaded file)
 wget -O newname.zip https://URL.domain/file.zip --progress=bar:force:noscroll
+
+#Shell hotkeys:
+#Go to beginning of line:
+ctrl+a
+#Go to end of line:
+ctrl+e
+
+##Vi hotkeys:
+#Go to top of file:
+gg
+#Go to bottom of file:
+shift+g
+#Delete a line:
+dd
