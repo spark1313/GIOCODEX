d74ca124b4cbeebd91f7f285cffc41356697735a
spark1313 <57932291+spark1313@users.noreply.github.com>
2023-05-24 16:59:44 -0400

    Create Pasta.sh

---
+++ Pasta/Pasta.sh
@@ -0,0 +1,5 @@
+#Run a command but continue running even if disconnected, with all output sent to nohup.out in the local directory"
+nohup <command>
+
+#Follow a log file line-by-line as it is generated
+tail -f file.log
