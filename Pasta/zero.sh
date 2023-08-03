e0630586d3544c804e8dd52b0cd7cfd0a183d116
spark1313 <57932291+spark1313@users.noreply.github.com>
2023-08-02 21:02:24 -0400

    Update zero.sh

--- Pasta/zero.sh
+++ Pasta/zero.sh
@@ -1,4 +1,11 @@
 #Write zeros to every sector of a drive:
-dd if=/dev/zero of=/dev/sdX bs=8M
+sudo dd if=/dev/zero of=/dev/sdX bs=8M
 #Verify drive has been zeroed out:
 sudo dd if=/dev/sdX bs=1M status=progress | od | head
+
+#Show badblocks info:
+sudo badblocks -v /dev/sdX
+#Write all zeros to a drive and confirm all bytes are zero:
+sudo badblocks -svw -t 0x00 /dev/sdX
+#Confirm all bytes written to a drive are zero'd out:
+sudo badblocks -sv -t 0x00 /dev/sdX
