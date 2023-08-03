43615948557d8c30fecd43434d8c4cfe1a58ef89
spark1313 <57932291+spark1313@users.noreply.github.com>
2023-08-02 21:09:14 -0400

    Create sequel.sql

---
+++ Pasta/sequel.sql
@@ -0,0 +1,16 @@
+--Export a PostgreSQL database table to CSV:
+COPY table_name TO '/directory/path/file.csv' WITH DELIMITER ',' CSV HEADER;
+or
+\copy (SELECT * FROM table_name) to 'C:\directory\path\file.csv' with csv
+
+--Import a CSV into a PostgreSQL database table:
+COPY table_name(column1_name, column2_name, column3_name) FROM '/directory/path/file.csv' WITH DELIMITER ',' CSV HEADER;
+
+--SQL to add text to cells: 
+UPDATE tablename SET columnname = CONCAT(columnname, "string") WHERE condition IS status;
+
+--SQL to convert a column to lower case:
+UPDATE tablename SET columnname = LOWER(othercolumnname);
+
+--SQL to find cells which contain a specific string:
+SELECT * FROM tablename WHERE columnname LIKE "%stringname%";
