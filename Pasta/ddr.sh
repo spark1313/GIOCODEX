3353142bc7a0189248f9bbd0cf50a4c1de81fc22
spark1313 <57932291+spark1313@users.noreply.github.com>
2023-08-02 22:26:30 -0400

    Update ddr.sh

--- Pasta/ddr.sh
+++ Pasta/ddr.sh
@@ -16,3 +16,5 @@ dd_rhelp --version
 
 #Rip disk to image:
 dd_rhelp /dev/sdX /directory/path/rescueAttempt.img
+
+#Alternatively, dd can be used directly for this to write contents out to a file. You can tell it to copy all the good data off the drive, then you can run it a few more times to go and try harder (i.e., use smaller block sizes) trying to pull data off the bad areas.
