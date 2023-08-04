2200ba0b4eb09b5953103c9750f0d9949c0ae593
spark1313 <57932291+spark1313@users.noreply.github.com>
2023-08-03 21:24:40 -0400

    Update benchmark.sh

--- Pasta/benchmark.sh
+++ Pasta/benchmark.sh
@@ -4,4 +4,8 @@ sudo hdparm -Tt /dev/sdX
 #Run badblocks test in read-only non-destructive mode:
 sudo badblocks -nvs /dev/sdX
 
-#Insert here steps with dd
+#Measure write speed with dd (Adjust blocksize bs and count as needed, if the target drive does not have a filesystem instead use of=/sdX)
+#Sequential:
+sudo dd if=/dev/zero of=/tmp/benchmark-test.img bs=1G count=1 oflag=dsync && sudo rm -f /tmp/benchmark-test.img
+#Latency:
+sudo dd if=/dev/zero of=/tmp/benchmark-test.img bs=512 count=1000 oflag=dsync && sudo rm -f /tmp/benchmark-test.img
