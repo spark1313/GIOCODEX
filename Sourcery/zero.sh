efbd6ec9430870143cc2400304ca0d5272f5488c
spark1313 <57932291+spark1313@users.noreply.github.com>
2023-12-14 01:03:07 -0500

    Update zero.sh

--- Sourcery/zero.sh
+++ Sourcery/zero.sh
@@ -3,9 +3,14 @@ sudo dd if=/dev/zero of=/dev/sdX bs=8M
 #Verify drive has been zeroed out:
 sudo dd if=/dev/sdX bs=1M status=progress | od | head
 
+
 #Show badblocks info:
 sudo badblocks -v /dev/sdX
+
 #Write all zeros to a drive and confirm all bytes are zero:
 sudo badblocks -svw -t 0x00 /dev/sdX
+#Above but fixed for drives larger than 17TB since the byte value for badblocks is only a 32-bit integer:
+sudo badblocks -svw -t 0x00 -b 8192 /dev/sdX
+
 #Confirm all bytes written to a drive are zero'd out:
 sudo badblocks -sv -t 0x00 /dev/sdX
