01f1808c4582d94984237e8ef372b915301d55c3
spark1313 <57932291+spark1313@users.noreply.github.com>
2023-12-21 17:31:49 -0500

    Update sequel.sql

--- Sourcery/sequel.sql
+++ Sourcery/sequel.sql
@@ -52,3 +52,6 @@ SELECT table1value FROM table1 INNER JOIN table2 on table1.table1value = table2.
 
 --Delete based on values in another table
 DELETE FROM table1 t USING table2 s WHERE t.table1value = s.table2value;
+
+--Rename a database: (Log in via psql -U postgres)
+ALTER DATABASE dbname RENAME TO newdbname
