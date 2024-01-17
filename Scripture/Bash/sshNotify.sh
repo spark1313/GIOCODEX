a0c3f24487f589192262bf5febef196faf665fec
spark1313 <57932291+spark1313@users.noreply.github.com>
2024-01-17 16:06:30 -0500

    Update sshNotify.sh
    Added an extra loop to limit executing this to only actual logins

--- Scripture/Bash/sshNotify.sh
+++ Scripture/Bash/sshNotify.sh
@@ -14,21 +14,30 @@
 BASEURL='notify.domain.com'
 TOKEN='H4HG0771M'
 
-#The script triggers if the SSH_CLIENT variable exists:
+#Get date of last SSH login, current date, and the difference between them in Epoch time:
+LASTSSHLOGINEPOCH=`date -d "$(who | grep pts | tail -1 | awk '{print $3, $4}')" +%s`
+CURRENTEPOCH=`date +%s`
+SINCELASTLOGINEPOCH=$(($CURRENTEPOCH-$LASTSSHLOGINEPOCH))
+
+#Trigger if the SSH_CLIENT variable exists:
 if [ -n "$SSH_CLIENT" ]; then
-        #Define the login time from date command:
-        LOGINTIME="$(date "+%Y.%m.%d %H:%M")"
-        #Pull in the client IP via the SSH_CLIENT variable:
-        CLIENTIP=$(echo $SSH_CLIENT | awk '{print $1}')
-        #Pull in the port via the SSH_CLIENT variable:
-        PORT=$(echo $SSH_CLIENT | awk '{print $3}')
-        #Pull in the server IP from the hostname command:
-        SERVERIP=$(hostname -I | awk '{print $1}')
-        #Define the notification title string, passing in crafted variables:
-        TITLE="SSH Login Alert for ${USER}@${HOSTNAME}"
-        #Define the message content string, passing in crafted variables:
-        MESSAGE="${USER} logged in via SSH on ${HOSTNAME} at $SERVERIP over port $PORT from $CLIENTIP on $LOGINTIME"
-        #Send a preformatted silent failsafe curl POST request in the background with one retry attempt, passing the output into /dev/null with the notification parameters passed in and variables inserted:
-        curl -X POST -fs -o /dev/null 2>&1 --retry 1 -F "title=${TITLE}" -F "message=${MESSAGE}" -F "priority=7" "https://${BASEURL}/message?token=${TOKEN}" &
-#fye fo fum end the loop
+        #Further trigger if the last SSH login was less than 2 minutes ago, some additional time than would be expected is allocated here to handle odd time delays:
+        if (($SINCELASTLOGINEPOCH <= 120)); then
+                #Define the login time from date command:
+                LOGINTIME="$(date "+%Y.%m.%d %H:%M")"
+                #Pull in the client IP via the SSH_CLIENT variable:
+                CLIENTIP=$(echo $SSH_CLIENT | awk '{print $1}')
+                #Pull in the port via the SSH_CLIENT variable:
+                PORT=$(echo $SSH_CLIENT | awk '{print $3}')
+                #Pull in the server IP from the hostname command:
+                SERVERIP=$(hostname -I | awk '{print $1}')
+                #Define the notification title string, passing in crafted variables:
+                TITLE="SSH Login Alert for ${USER}@${HOSTNAME}"
+                #Define the message content string, passing in crafted variables:
+                MESSAGE="${USER} logged in via SSH on ${HOSTNAME} at $SERVERIP over port $PORT from $CLIENTIP on $LOGINTIME"
+                #Send a preformatted silent failsafe curl POST request in the background with one retry attempt, passing the output into /dev/null with the notification parameters passed in and variables inserted:
+                curl -X POST -fs -o /dev/null 2>&1 --retry 1 -F "title=${TITLE}" -F "message=${MESSAGE}" -F "priority=7" "https://${BASEURL}/message?token=${TOKEN}" &
+        #fye fo fum
+        fi
+#End the if loops:
 fi
