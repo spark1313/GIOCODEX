5f8acb4e76e80f53019e29fd26583a3f28653462
spark1313 <57932291+spark1313@users.noreply.github.com>
2024-05-17 15:28:34 -0400

    Update shellcustomizations.sh

--- Sourcery/shellcustomizations.sh
+++ Sourcery/shellcustomizations.sh
@@ -1,6 +1,7 @@
-#This file identifies shell customizations which are applied to all users of the system and sourced upon boot.
-#Create the file in /etc/profile.d/shellcustomizations.sh
-#If updating the file, you will need to re-source the file for it to apply before a reboot via 'source /etc/profile'
+#This file identifies shell customizations which are applied to all users of the system.
+#Add these lines to /etc/bash.bashrc for use in interactive shells
+#Then to also apply to login shells, create the file '/etc/profile.d/bashrcsourcer.sh' with the line 'source /etc/bash.bashrc'
+#If updating the lines, you will need to re-source the file for it to apply before a reboot via 'source /etc/profile'
 alias ll="ls -lath"
 alias l="ls -l"
 alias ..="cd .."
