bb60ea92f9cdd85765a6e1fca0004f4932b3f038
spark1313 <57932291+spark1313@users.noreply.github.com>
2024-12-01 23:10:35 -0500

    Update mysql-db-automated-backup.sh

--- Scripture/Bash/mysql-db-automated-backup.sh
+++ Scripture/Bash/mysql-db-automated-backup.sh
@@ -1,1 +1,4 @@
-
+#This is desifgned to be ran via Unraid Custom User Scripts on MariaDB docker container databases
+#!/bin/bash
+mkdir /mnt/user/share/Backups/Application/DB/$(date +"%Y.%m.%d")
+docker exec mariadb mysqldump -h 192.168.IP.ADDR -u dbusername -ppasswordgoeshere dbname > /mnt/user/share/Backups/Application/DB/$(date +"%Y.%m.%d")/application-db-backup-$(date +"%Y.%m.%d").sql
