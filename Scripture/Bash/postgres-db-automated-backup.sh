db1e159cffa194bfafd87816b0ac898ff91626d0
spark1313 <57932291+spark1313@users.noreply.github.com>
2024-12-16 15:05:10 -0500

    Update postgres-db-automated-backup.sh

--- Scripture/Bash/postgres-db-automated-backup.sh
+++ Scripture/Bash/postgres-db-automated-backup.sh
@@ -1,5 +1,24 @@
-#This is desifgned to be ran via Unraid Custom User Scripts on PostgreSQL docker container databases
-#Be sure that the PGPASSWORD environment variable / container variable is already set with the correct password
 #!/bin/bash
-mkdir /mnt/user/share/Backups/Application/DB/$(date +"%Y.%m.%d")
-docker exec postgresql14 pg_dump -h 192.168.IP.ADDR -U dbusername dbname > /mnt/user/share/Backups/Application/DB/$(date +"%Y.%m.%d")/application-db-backup-$(date +"%Y.%m.%d").sql
+#This is designed to be ran via Unraid Custom User Scripts on PostgreSQL docker container databases
+#Be sure that the PGPASSWORD environment variable / container variable is already set with the correct password
+
+#Define info about the mariadb/mysql database backing up
+DBHOST='192.168.IP.ADDR'
+DBNAME='dbnamegoeshere'
+DBUSER='dbusergoeshere'
+
+#OPTIONAL: Input your Gotify base URL here as well as the app token
+BASEURL='notify.domain.com'
+TOKEN='H4HG0771M'
+
+#Define the path where planning to save the backup
+SAVEPATH='/mnt/user/share/Backups/Application/DB/'
+
+#First create the directory in case it goes not yet exist
+mkdir -p $SAVEPATH
+
+#Run the backup from within the corresponding docker container
+docker exec postgresql14 pg_dump -h $DBHOST -U $DBUSER $DBNAME > $SAVEPATH/application-db-backup-$(date +"%Y.%m.%d").sql
+
+#OPTIONAL: Send a Gotify notification on script execution
+curl -X POST -fs -o /dev/null 2>&1 --retry 1 -F "title=$DBHOST database backup script completed" -F "message=Database backup completed and saved to $SAVEPATH" -F "priority=7" "https://${BASEURL}/message?token=${TOKEN}" &> /dev/null
